d5c0833a4a55df30ad15910bcbc9bda8c73c3289
sergey.koroteev <dhonehi@gmail.com>
2021-11-25 20:31:44 +0300

    removed vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import Vuex from "vuex";
 import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
-app.use(Vuex)
-   .use(router)
+app.use(router)
    .use(ElementPlus)
    .mount('#app')
