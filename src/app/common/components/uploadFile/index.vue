0dd8531e2d2f5d5d7264d57aeb4f980429da35f6
Herman461 <zhukovskiyherman@gmail.com>
2021-12-08 17:35:19 +0300

    v0.1

---
+++ src/app/common/components/uploadFile/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="select-file">
+
+    <div class="select-file__button">
+      <img class="button-select-file__icon" src="@assets/images/icons/tasks/select.svg">
+      <button class="button-select-file__content"></button>
+    </div>
+    <div class="select-file__desc">Or drag and drop the file here</div>
+    <div class="select-file__prompt">You can upload up to 10 files with a maximum size of 100MB</div>
+
+
+  </div>
+</template>
