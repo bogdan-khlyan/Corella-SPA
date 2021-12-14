4cfa60e3531601ab063dcae632c937c06e9eea9f
CompUser <knkotov37@gmail.com>
2021-12-14 16:20:18 +0300

    added pagination user management

--- src/app/userManagement/index.vue
+++ src/app/userManagement/index.vue
@@ -9,32 +9,45 @@
       </button>
     </div>
 
-    <el-table :data="dataTable">
+    <div class="user-management__table">
+      <el-table :data="dataTable">
 
-      <el-table-column prop="name" label="Name" min-width="200">
-        <template #default="scope">
-          <img class="user-management__avatar" src="@/assets/images/corella_icon.svg" alt="">
-          {{scope.row.name}}
-        </template>
-      </el-table-column>
+        <el-table-column prop="name" label="Name" min-width="200">
+          <template #default="scope">
+            <img class="user-management__avatar" src="@/assets/images/corella_icon.svg" alt="">
+            {{scope.row.name}}
+          </template>
+        </el-table-column>
 
-      <el-table-column prop="email" label="E-mail" min-width="200"/>
+        <el-table-column prop="email" label="E-mail" min-width="200"/>
 
-      <el-table-column prop="isBan" label="Ban" width="200px">
-        <template #default="scope">
-          <el-switch v-model="scope.row.isBan" active-color="#F32B2A" inactive-color="#BDBCC8" />
-        </template>
-      </el-table-column>
+        <el-table-column prop="isBan" label="Ban" width="200px">
+          <template #default="scope">
+            <el-switch v-model="scope.row.isBan" active-color="#F32B2A" inactive-color="#BDBCC8" />
+          </template>
+        </el-table-column>
 
-      <el-table-column fixed="right" label="Actions" width="200px">
-        <template #default="scope">
-          <button @click="editUser(scope.row.id)" class="user-management__edit-btn">
-            <img src="@/assets/images/icons/buttons/icon-edit.svg" alt="add">
-          </button>
-        </template>
-      </el-table-column>
+        <el-table-column fixed="right" label="Actions" width="230px">
+          <template #default="scope">
+            <button @click="editUser(scope.row.id)" class="user-management__edit-btn">
+              <img src="@/assets/images/icons/buttons/icon-edit.svg" alt="add">
+            </button>
+          </template>
+        </el-table-column>
 
-    </el-table>
+      </el-table>
+    </div>
+
+    <div class="user-management__pagination">
+      <el-pagination
+          v-model:currentPage="currentPage3"
+          :page-size="10"
+          layout="prev, pager, next, jumper"
+          :total="41"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"/>
+    </div>
+
   </div>
 </template>
 
