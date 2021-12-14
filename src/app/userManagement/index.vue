02bdf3e6b24b96928dcd599896b14d549cf0e386
CompUser <knkotov37@gmail.com>
2021-12-14 15:36:05 +0300

    create table user management

--- src/app/userManagement/index.vue
+++ src/app/userManagement/index.vue
@@ -1,27 +1,100 @@
 <template>
   <div class="user-management">
-    <span>User management</span>
+
+    <div class="user-management__header">
+      <div class="user-management__header-title">User Management</div>
+      <button @click="openModal">
+        <img src="@/assets/images/icons/buttons/icon-plus.svg" alt="add">
+        Add user
+      </button>
+    </div>
+
+    <el-table :data="dataTable">
+
+      <el-table-column prop="name" label="Name" min-width="200">
+        <template #default="scope">
+          <img class="user-management__avatar" src="@/assets/images/corella_icon.svg" alt="">
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="email" label="E-mail" min-width="200"/>
+
+      <el-table-column prop="isBan" label="Ban" width="200px">
+        <template #default="scope">
+          <el-switch v-model="scope.row.isBan" active-color="#F32B2A" inactive-color="#BDBCC8" />
+        </template>
+      </el-table-column>
+
+      <el-table-column fixed="right" label="Actions" width="200px">
+        <template #default="scope">
+          <button @click="editUser(scope.row.id)" class="user-management__edit-btn">
+            <img src="@/assets/images/icons/buttons/icon-edit.svg" alt="add">
+          </button>
+        </template>
+      </el-table-column>
+
+    </el-table>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'user-management'
+  name: 'user-management',
+  data(){
+    return {
+      dataTable:[
+        {
+          id: 0,
+          avatar: null,
+          name: 'Lana',
+          email: 'superadmin2021@gmail.com',
+          isBan: false,
+        },
+        {
+          id: 1,
+          avatar: null,
+          name: 'Lana',
+          email: 'superadmin2021@gmail.com',
+          isBan: true,
+        },
+        {
+          id: 2,
+          avatar: null,
+          name: 'Lana',
+          email: 'superadmin2021@gmail.com',
+          isBan: false,
+        },
+        {
+          id: 3,
+          avatar: null,
+          name: 'Lana',
+          email: 'superadmin2021@gmail.com',
+          isBan: false,
+        },
+        {
+          id: 4,
+          avatar: null,
+          name: 'Lana',
+          email: 'superadmin2021@gmail.com',
+          isBan: false,
+        }
+      ]
+    }
+  },
+  methods: {
+    openModal(){
+      console.log('openModal')
+      alert('openModal')
+    },
+    editUser(data) {
+      console.log(data)
+    }
+  }
 }
 </script>
 
-<style scoped lang="scss">
-.user-management {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 100%;
-  height: 100%;
-
-  font-size: 48px;
-  font-weight: 500;
-
-  color: #0AB258;
-}
+<style lang="scss">
+@import "src/assets/scss/user-management.scss";
 </style>
