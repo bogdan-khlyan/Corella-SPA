97af4ee2e5dc035ed1bd17e50cd2691f68ff6bc9
bogdan.hlyan <es.bogdan.hlyan@gmail.com>
2021-12-18 19:27:09 +0300

    added fullscreen drag and drop to BaseWrapper

--- src/app/common/baseWrapper/BaseWrapper.vue
+++ src/app/common/baseWrapper/BaseWrapper.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="base-wrapper" :style="styles">
+  <div class="base-wrapper"
+       :style="styles"
+       @dragover.prevent="dragOver"
+       @drop.prevent="dropFile"
+       @mouseleave="dragLeave">
     <div class="base-wrapper__sidebar"
          :class="{'base-wrapper__sidebar--collapsed':isCollapse}">
       <base-sidebar/>
@@ -10,6 +14,17 @@
         <base-header/>
       </div>
       <div class="base-wrapper__view">
+
+        <transition name="el-fade-in-linear" mode="out-in">
+          <div v-show="drag"
+               class="base-wrapper__drag-and-drop">
+            <div class="base-wrapper__drag-and-drop--content">
+              <img src="@/assets/images/icons/common/icon-file.svg" alt="">
+              <span>Drag and drop the file here</span>
+            </div>
+          </div>
+        </transition>
+
         <router-view v-slot="{ Component }">
           <transition name="el-fade-in-linear" mode="out-in">
             <component :is="Component" />
@@ -25,6 +40,7 @@ import BaseHeader from "@/app/common/baseWrapper/baseHeader/BaseHeader";
 import BaseSidebar from "@/app/common/baseWrapper/baseSidebar/BaseSidebar";
 import sidebarCollapse from "@/app/common/baseWrapper/baseSidebar/sidebar-mixin";
 import {baseWrapperConfig} from "@/app/common/baseWrapper/base-wrapper.config";
+import {baseWrapperState} from "@/app/common/baseWrapper/base-wrapper.state";
 
 export default {
   name: 'base-wrapper',
@@ -41,6 +57,23 @@ export default {
       } else {
         return baseWrapperConfig.get('default').styles
       }
+    }
+  },
+  data() {
+    return {
+      drag: false
+    }
+  },
+  methods: {
+    dragOver() {
+      this.drag = true
+    },
+    dragLeave() {
+      this.drag = false
+    },
+    dropFile(event) {
+      this.drag = false
+      baseWrapperState.dragFiles = event.dataTransfer.files
     }
   }
 }
@@ -67,9 +100,53 @@ export default {
   }
 
   &__view {
+    position: relative;
+
     padding: 20px;
     box-sizing: border-box;
     min-height: calc(100vh - 70px);
+  }
+
+  &__drag-and-drop {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
+
+    z-index: 2;
+
+    background: rgba(60, 75, 97, 0.6);
+    backdrop-filter: blur(5px);
+
+    display: flex;
+    align-items: center;
+
+    &--content {
+      width: max-content;
+      margin: 0 auto;
+
+      > img {
+        margin:  20px auto;
+      }
+
+      > span {
+        font-family: Rubik, sans-serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 32px;
+        line-height: 42px;
+
+        display: flex;
+        align-items: center;
+        text-align: center;
+
+        color: #FFFFFF;
+      }
+
+    }
+
   }
 
   &__header {
